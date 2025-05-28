0a97195c07af5528d47873df38d501c89d99ad2c
Jatin <102823693+jatin-jamdagni@users.noreply.github.com>
2025-05-28 12:46:02 +0000

    done

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export async function GET(request: NextRequest) {
+export async function GET(_request: NextRequest) {
     return NextResponse.json({ gotIt: 'GoodToKnow' });
 }
