5e26c85ab3c67dc7b1eb57239608616dbd9ba9d8
Jatin <102823693+jatin-jamdagni@users.noreply.github.com>
2025-05-28 12:56:26 +0000

    done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,19 @@
-import { AboutSection } from "@/components/about-section";
-import { FooterSection } from "@/components/footer-section";
-import HeroSection from "@/components/hero-section";
-import { MetricsSection } from "@/components/metrix-section";
-import { SpecializationSection } from "@/components/specialization-section";
+// import { AboutSection } from "@/components/about-section";
+// import { FooterSection } from "@/components/footer-section";
+// import HeroSection from "@/components/hero-section";
+// import { MetricsSection } from "@/components/metrix-section";
+// import { SpecializationSection } from "@/components/specialization-section";
 import React from "react";
 
 const Home = () => {
   return (
     <div>
-      <HeroSection />
+      {/* <HeroSection />
       <MetricsSection />
       <AboutSection />
       <SpecializationSection />
-      <FooterSection />
+      <FooterSection /> */}
+      <strong>Nana Krte pyar mein to kargyi kargyi</strong>
     </div>
   );
 };
